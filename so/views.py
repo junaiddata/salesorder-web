c8138b26e0ce9bdcd7df092a5504c9095d3994ea
rashadptp <junaiddataanalyst@gmail.com>
2025-12-22 11:11:53 +0400

    AAA

--- so/views.py
+++ so/views.py
@@ -3873,7 +3873,7 @@ def export_so_pdf(request):
     buffer.seek(0)
     
     response = HttpResponse(buffer, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="sales_orders.pdf"'
+    response['Content-Disposition'] = 'inline; filename="sales_orders.pdf"'
     return response
 
 
